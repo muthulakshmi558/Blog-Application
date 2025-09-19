4143bb3b50b86a3818b2a83e3a0254c315971039
Muthulakshmi <muthulakshmi5293@gmail.com>
2025-09-19 08:33:43 +0530

    Update urls.py

--- backend/blog_project/urls.py
+++ backend/blog_project/urls.py
@@ -18,10 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.authtoken.views import ObtainAuthToken
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('blog.urls')),
+    path('auth/token/', ObtainAuthToken.as_view(), name='login'),
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
